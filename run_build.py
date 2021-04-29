4ee8b90c7da8fa6ae98301421df477bbb9113224
Soramichi Akiyama <akiyama@m.soramichi.jp>
2021-04-29 15:31:15 +0900

    merge various settings into one file

--- run_build.py
+++ run_build.py
@@ -5,9 +5,10 @@ import urllib.request
 import shutil
 import os
 import smtplib
+import json
 from pathlib import Path
 from email.message import EmailMessage
-from typing import List, Match
+from typing import List, Dict, Any
 
 def force_delete(path: str):
     try:
@@ -37,14 +38,6 @@ def newer(v1: str, v2: str) -> int:
     else:
         return -1
 
-def get_lock_ver() -> str:
-    try:
-        with open("./lockfile", "r") as f:
-            return f.readline().split("\n")[0] # remove tailing '\n'
-    except FileNotFoundError:
-        # if the lockfile does not exit, returns an empty string
-        return ""
-
 def find_latest_ver(lock_ver: str) -> str:
     git_ls_remote: List[str] = subprocess.check_output(["git", "ls-remote", "--tags", "--ref"], text=True, cwd="./git", stderr=subprocess.DEVNULL).split('\n')[:-1]
     avail_vers: List[str] = []
@@ -60,10 +53,6 @@ def find_latest_ver(lock_ver: str) -> str:
 
     return sorted(avail_vers, key=functools.cmp_to_key(newer))[-1]
 
-def get_built_ver() -> str:
-    with open("./built_version", "r") as f:
-        return f.readline().split("\n")[0] # remove tailing '\n'
-
 def create_url(v: str) -> str:
     URL_BASE: str = "https://cdn.kernel.org/pub/linux/kernel/v"
     major_ver: str = v.split('.')[0]
@@ -71,7 +60,7 @@ def create_url(v: str) -> str:
     # Ex: https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.11.15.tar.xz
     return URL_BASE + major_ver + ".x/linux-" + v + ".tar.xz"
 
-def do_build(v: str):
+def do_build(v: str) -> bool:
     url: str = create_url(v)
     dirname: str = "linux-" + v
     filename: str = dirname + ".tar.xz"
@@ -105,46 +94,47 @@ def do_build(v: str):
         f.write(output)
 
     if output.find("error:") == -1 and output.find("Error:") == -1:
-        print("Build success!")
         shutil.copyfile(dirname + "/.config", "config")   # copy the latest config back
-        with open("built_version", "w") as f:             # remember v as the latest built version
-            f.write(str(v))
-        force_delete(filename)
-        force_delete(dirname)
+        # force_delete(filename)
+        # force_delete(dirname)
+        return True
     else:
-        print("An error occured while buidling. Please check out the log.")
+        return False
 
-def notify(ver: str):
-    try:
-        with open("mail_config") as f:
-            to_addr: str = f.readline().split("\n")[0]
-            from_addr: str = f.readline().split("\n")[0]
-            server_addr: str = f.readline().split("\n")[0]
-            user: str = f.readline().split("\n")[0]
-            pswd: str = f.readline().split("\n")[0]
-            port: int = int(f.readline().split("\n")[0])
-    except FileNotFoundError:
-        # do nothing if mail_config does not exit
-        return
-
+def notify(ver: str, mail_config: Dict[str, str]):
     msg = EmailMessage()
     msg['Subject'] = ('New kernel was successfully built: ' + ver)
-    msg['From'] = from_addr
-    msg['To'] = to_addr
+    msg['From'] = mail_config["from_addr"]
+    msg['To'] = mail_config["to_addr"]
 
-    s = smtplib.SMTP(server_addr, port)
+    s = smtplib.SMTP(mail_config["server_addr"], int(mail_config["port"]))
     s.starttls()
-    s.login(user, pswd)
+    s.login(mail_config["user"], mail_config["password"])
     s.send_message(msg)
 
 if __name__ == "__main__":
-    lock_ver: str = get_lock_ver()
-    latest_ver: str = find_latest_ver(lock_ver)
-    built_ver: str = get_built_ver()
+    with open("setting.json", "r") as f:
+        setting: Dict[str, Any] = json.load(f)
 
+    lock_ver: str = ""
+    if "lock_ver" in setting:
+        lock_ver = setting["lock_ver"]
+    latest_ver: str = find_latest_ver(lock_ver)
+    built_ver: str = setting["built_ver"]
+    
     if newer(latest_ver, built_ver) > 0:
         print("A new version available:", latest_ver)
-        do_build(latest_ver)
-        notify(latest_ver)
+
+        ret: bool = do_build(latest_ver)
+        if ret:
+            print("Build success!")
+            setting["built_ver"] = latest_ver
+            with open("setting.json", "w") as f: # remember v as the latest built version
+                f.write(json.dumps(setting, indent=4))
+        else:
+            print("An error occured while buidling. Please check out the log.")            
+
+        if "mail_config" in setting:
+            notify(latest_ver, setting["mail_config"])
     else:
         print("No new version is available.")
